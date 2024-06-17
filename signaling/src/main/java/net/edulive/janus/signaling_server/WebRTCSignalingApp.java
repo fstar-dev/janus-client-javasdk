313c0e934ea5268f52ee46d42c6b01c5aed65450
groovy <groovyrocome@gmail.com>
2024-06-17 04:32:43 -0400

    fix: updated WebRTCSignalingApp

--- signaling/src/main/java/net/edulive/janus/signaling_server/WebRTCSignalingApp.java
+++ signaling/src/main/java/net/edulive/janus/signaling_server/WebRTCSignalingApp.java
@@ -4,104 +4,90 @@ import io.javalin.Javalin;
 import io.javalin.config.JettyConfig;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.websocket.WsContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
-import org.eclipse.jetty.server.SslConnectionFactory;
-import org.eclipse.jetty.http.HttpVersion;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
 
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import io.javalin.community.ssl.SslPlugin;
 
 public class WebRTCSignalingApp {
-    private static final Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
+	private static final Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
 
-    private static final Logger logger = LoggerFactory.getLogger(WebRTCSignalingApp.class);
+	private static final Logger logger = LoggerFactory.getLogger(WebRTCSignalingApp.class);
 
-    public static void main(String[] args) {
-    	
-    	Javalin app = Javalin.create(config ->
-                config.staticFiles.add("/main/resources/public", Location.CLASSPATH)
-        );
-        
-        // SSL Context Factory for HTTPS configuration
-        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
-//        sslContextFactory.setKeyStorePath("c:\\Program Files\\nginx-1.26.1\\nginx-1.26.1\\keystore.jks");
-        sslContextFactory.setKeyStorePath("/main/resources/keystore.jks");
-        sslContextFactory.setKeyStorePassword("QWE@#$qwe234");
-        
-        sslContextFactory.setKeyStorePath("/main/resources/keystore.jks");
-    	sslContextFactory.setKeyStorePassword("QWE@#$qwe234");
-    	sslContextFactory.setKeyManagerPassword("QWE@#$qwe234");
+	public static void main(String[] args) {
 
-        // HTTPS Configuration
-        HttpConfiguration httpsConfig = new HttpConfiguration();
-        httpsConfig.addCustomizer(new SecureRequestCustomizer());
-        
-        // Create an HTTPS connector
-        ServerConnector sslConnector = new ServerConnector( app.jettyServer().server(),
-                new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
-                new HttpConnectionFactory(httpsConfig));
-        sslConnector.setHost("0.0.0.0");
-        sslConnector.setPort(1443);
+		// SslPlugin sslPlugin = new SslPlugin(conf -> {
+		// 	conf.pemFromPath("c:\\fullchain1.pem", "c:\\privkey1.pem");
+		// });
+//    	Javalin app = Javalin.create(config ->
+////    		config.plugins.
+//
+//);
+		Javalin app = Javalin.create(javalinConfig -> {
+//    	    javalinConfig.registerPlugin(sslPlugin);
+			javalinConfig.staticFiles.add("/main/resources/public");
+		});
+//        // SSL Context Factory for HTTPS configuration
+//        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+//        sslContextFactory.setKeyStorePath("C:\\Program Files\\Java\\jdk-21.0.2\\lib\\security\\keystore.jks");
+//        sslContextFactory.setKeyStorePassword("123456");
+//        sslContextFactory.setEndpointIdentificationAlgorithm("HTTPS");
+//
+//        // HTTPS Configuration
+//        HttpConfiguration httpsConfig = new HttpConfiguration();
+//        httpsConfig.addCustomizer(new SecureRequestCustomizer());
 
-        app.get("/public", ctx -> ctx.redirect("/index.html"));
-        app.jettyServer().server().addConnector(sslConnector);
-        // Add the connector to the server
-        app.start();
-        
-	
-        MessageHandler messageHandler = new MessageHandler(System.getProperty("janus_address", "ws://49.13.235.28:8188"));
+		// Create an HTTPS connector
+//        ServerConnector sslConnector = new ServerConnector( app.jettyServer().server(),
+//                new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+//                new HttpConnectionFactory(httpsConfig));
+//        sslConnector.setHost("0.0.0.0");
+//        sslConnector.setPort(2443);
+//        app.jettyServer().server().addConnector(sslConnector);
 
-        app.ws("/signaling", ws -> {
-            ws.onConnect(ctx -> {
-                String username = randomString();
-                userUsernameMap.put(ctx, username);
-                messageHandler.createSession(username, ctx);
-                ctx.send("{\"type\":\"status\",\"status\":\"connected\"}");
-                logger.info("{} joined", username);
-            });
-            ws.onClose(ctx -> {
-                String username = userUsernameMap.get(ctx);
-                userUsernameMap.remove(ctx);
-                messageHandler.destroySession(username);
-                logger.info("{} left ", username);
-            });
-            ws.onMessage(ctx -> {
-                String username = userUsernameMap.get(ctx);
-                logger.info("{} send {}", username, ctx.message());
-                messageHandler.handleMessage(userUsernameMap.get(ctx), ctx);
-            });
-        });
-    }
+		app.get("/public", ctx -> ctx.redirect("/index.html"));
+		// Add the connector to the server
+		app.start(7070);
 
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final Random random = new Random();
-    
-    private static void configureServer(JettyConfig config) {
-        config.server(() -> {
-            // Create a basic Javalin Jetty server
-            Server server = new Server();
+		MessageHandler messageHandler = new MessageHandler(
+				System.getProperty("janus_address", "ws://49.13.235.28:8188"));
 
-        
-            return server;
-        });
-    }
-    public static String randomString() {
-        int length = 20; // the desired length of the random string
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(CHARACTERS.length());
-            sb.append(CHARACTERS.charAt(index));
-        }
-        return sb.toString();
-    }
+		app.ws("/api", ws -> {
+			ws.onConnect(ctx -> {
+				String username = randomString();
+				userUsernameMap.put(ctx, username);
+				messageHandler.createSession(username, ctx);
+				ctx.send("{\"type\":\"status\",\"status\":\"connected\"}");
+				logger.info("{} joined", username);
+			});
+			ws.onClose(ctx -> {
+				String username = userUsernameMap.get(ctx);
+				userUsernameMap.remove(ctx);
+				messageHandler.destroySession(username);
+				logger.info("{} left ", username);
+			});
+			ws.onMessage(ctx -> {
+				String username = userUsernameMap.get(ctx);
+				logger.info("{} send {}", username, ctx.message());
+				messageHandler.handleMessage(userUsernameMap.get(ctx), ctx);
+			});
+		});
+	}
+
+	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private static final Random random = new Random();
+
+	public static String randomString() {
+		int length = 20; // the desired length of the random string
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
+			int index = random.nextInt(CHARACTERS.length());
+			sb.append(CHARACTERS.charAt(index));
+		}
+		return sb.toString();
+	}
 }
